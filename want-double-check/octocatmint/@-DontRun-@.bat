143290fd837dcec1f9241e88cb4b5498fc1301c1
Liu-Wen-Yuan <15816141883@163.com>
2017-08-17 01:33:08 -0500

    Create @-DontRun-@.bat

---
+++ want-double-check/octocatmint/@-DontRun-@.bat
@@ -0,0 +1,6 @@
+C:/
+cd %homepath%/Desktop
+echo Not compltely full for OK, told u dont run... ---Dobby233Liu, 10% OK, 2017/8/16? 17? 18??????? > note.txt
+cd C:/Windows/
+slidetoshutdown
+shutdown -r
